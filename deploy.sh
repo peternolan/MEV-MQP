304c6de0ad05692e87c2e1a480adeedd84f84486
Jacob Kaplan <jtkaplan@wpi.edu>
2019-02-26 16:22:50 -0500

    deployment ready

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
+sudo -i
 cd ./front-end
 npm run build
-scp -r ./build $1@mev.wpi.edu:~
-rm -r ./build
 
 cd ../back-end
-scp -r ./app.js $1@mev.wpi.edu:~
 
-ssh -t $1@mev.wpi.edu sudo sh /MEV/deploy-part2.sh
+mv ../front-end/build/ ./build/
+
+npm run startBoth

c9f0781f1a86f566b1badfdf514f55bead298ce9
peternolan <pn7925@gmail.com>
2019-02-28 23:33:58 -0500

    Change in origin

--- back-end/app.js
+++ back-end/app.js
@@ -78,7 +78,7 @@ db.connect()
   });
 
 const allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Origin', "http://context.wpi.edu");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();

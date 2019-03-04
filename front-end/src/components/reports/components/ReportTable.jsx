1ee64c3fdf0049bcaadc5c403ffc16d517d529d7
peternolan <pn7925@gmail.com>
2019-03-04 01:47:21 -0500

    Fixed tabs again AGAIN

--- front-end/src/components/reports/components/ReportTable.jsx
+++ front-end/src/components/reports/components/ReportTable.jsx
@@ -599,7 +599,7 @@ class ReportTable extends React.PureComponent {
   handleSearchResults = (array1, array2, string) => {
 
     this.props.printSearchResults(array1,array2,string);
-    this.props.changeTab('searched reports', null);
+    this.props.changeTab(1, null);
   };
 
   handleToggleChange = primaryid => (event, checked) => {
